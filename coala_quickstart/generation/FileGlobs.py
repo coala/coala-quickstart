53fe356c62722326fb0db85bae7fbde33228e101
Adhityaa C <c.adhityaa@gmail.com>
2016-06-21 12:13:33 +0530

    generation: Create glob expression from file list
    With `generate_glob_exps` we can generate concise glob expressions
    that match exactly the list of files given.

--- coala_quickstart/generation/FileGlobs.py
+++ coala_quickstart/generation/FileGlobs.py
@@ -40,3 +40,62 @@ def get_project_files(log_printer, printer, project_dir):
         ignored_file_paths=ignore_path_globs)
 
     return file_paths
+
+
+def generate_glob_exps(project_files, extensions, project_dir):
+    """
+    Generate glob expressions that matches exactly the files
+    with the given extensions in the user's project directory.
+
+    :param project_files:
+        The list of files to match.
+    :param extensions:
+        A list of file extensions.
+    :param project_dir:
+        The user's project directory.
+    :return:
+        A tuple with the first element (a list of glob expressions)
+        representing files to include and the second element (a list
+        of glob expressions) representing the files to exclude.
+    """
+    included_globs = []
+    excluded_globs = []
+
+    for root, dirs, files in os.walk(project_dir):
+        included_files = {}
+        excluded_files = {}
+        for file in files:
+            name, ext = os.path.splitext(file)
+            if ext not in extensions:
+                continue
+
+            file = os.path.join(root, file)
+            if file in project_files:
+                included_files.setdefault(ext, []).append(file)
+            else:
+                excluded_files.setdefault(ext, []).append(file)
+
+        for extension in included_files:
+            if extension not in excluded_files:
+                included_globs.append(os.path.join(root, "*" + extension))
+            else:
+                if (len(included_files[extension]) >=
+                        len(excluded_files[extension])):
+                    included_globs.append(os.path.join(root, "*" + extension))
+                    for excluded_file in excluded_files[extension]:
+                        excluded_globs.append(excluded_file)
+                    del excluded_files[extension]
+                else:
+                    for included_file in included_files[extension]:
+                        included_globs.append(included_file)
+                    del excluded_files[extension]
+
+        for extension in excluded_files:
+            excluded_globs.append(os.path.join(root, "*" + extension))
+
+    included_globs = [os.path.relpath(
+        included_glob, project_dir) for included_glob in included_globs]
+    excluded_globs = [os.path.relpath(
+        excluded_glob, project_dir) for excluded_glob in excluded_globs]
+
+    return included_globs, excluded_globs
--- tests/generation/FileGlobs.py
+++ tests/generation/FileGlobs.py
@@ -1,16 +1,19 @@
 import os
 import unittest
 
-from pyprint.ConsolePrinter import ConsolePrinter
+from pyprint.NullPrinter import NullPrinter
 from coalib.output.printers.LogPrinter import LogPrinter
 from coalib.misc.ContextManagers import (
     simulate_console_inputs, suppress_stdout, retrieve_stdout)
-from coala_quickstart.generation.FileGlobs import get_project_files
+from coala_quickstart.generation.FileGlobs import (
+    get_project_files, generate_glob_exps)
+from coalib.collecting.Collectors import collect_files
+
 
 class TestQuestion(unittest.TestCase):
 
     def setUp(self):
-        self.printer = ConsolePrinter()
+        self.printer = NullPrinter()
         self.log_printer = LogPrinter(self.printer)
 
     def test_get_project_files(self):
@@ -32,5 +35,67 @@ class TestQuestion(unittest.TestCase):
             self.assertIn(os.path.join(os.getcwd(), "root.c"), res)
             self.assertNotIn(os.path.join(os.getcwd(), "ignore_dir/src.c"), res)
             self.assertNotIn(os.path.join(os.getcwd(), "ignore_dir/src.js"), res)
+
+        os.chdir(orig_cwd)
+
+    def test_generate_glob_exps(self):
+        orig_cwd = os.getcwd()
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        os.makedirs("file_globs_gen_testfiles", exist_ok=True)
+        os.chdir("file_globs_gen_testfiles")
+
+        dir1 = os.path.join("src", "inc", "dir")
+        dir2 = os.path.join("src", "inc", "abs")
+        dir3 = os.path.join("src", "run", "dir")
+        dir4 = os.path.join("src", "run", "abs")
+        os.makedirs(dir1, exist_ok=True)
+        os.makedirs(dir2, exist_ok=True)
+        os.makedirs(dir3, exist_ok=True)
+        os.makedirs(dir4, exist_ok=True)
+
+        files = [os.path.join(dir1, "file1.c"),
+            os.path.join(dir1, "file2.c"),
+            os.path.join(dir2, "file3.c"),
+            os.path.join(dir2, "file4.c"),
+            os.path.join(dir3, "file5.c"),
+            os.path.join(dir4, "file9.c"),
+            os.path.join(dir4, "file10.c"),
+            os.path.join(dir1, "run.js")]
+        ignored_files = [os.path.join(dir3, "file6.c"),
+            os.path.join(dir3, "file7.c"),
+            os.path.join(dir4, "file8.c"),
+            os.path.join(dir4, "test.js"),
+            os.path.join(dir3, "run.py")]
+        # src/inc/dir and src/inc/abs have all files included
+        # thus src/inc is fully included
+        # src/run/dir has 1 inclusion, 2 exclusions
+        # src/run/abs has 2 inclusions, 1 exclusion
+
+        files = [os.path.abspath(file) for file in files]
+        ignored_files = [os.path.abspath(file) for file in ignored_files]
+        for file in files + ignored_files:
+            open(file, "w").close()
+
+        include_globs, exclude_globs = generate_glob_exps(
+            project_files=files,
+            extensions=[".c", ".js"],
+            project_dir=os.getcwd())
+
+        glob1 = os.path.join(dir1, "*.c")
+        glob2 = os.path.join(dir2, "*.c")
+        glob3 = os.path.join(dir3, "*.c")
+        glob4 = os.path.join(dir4, "*.c")
+        jsglob1 = os.path.join(dir1, "*.js")
+        jsglob4 = os.path.join(dir4, "*.js")
+        self.assertEqual(
+            sorted(include_globs),
+            sorted([jsglob1, glob1, glob2, glob4, os.path.join(dir3, "file5.c")]))
+        self.assertEqual(
+            sorted(exclude_globs),
+            sorted([jsglob4, os.path.join(dir4, "file8.c")]))
+
+        returned_files = {os.path.abspath(file) for file in collect_files(
+            include_globs, self.log_printer, ignored_file_paths=exclude_globs)}
+        self.assertEqual(sorted(returned_files), sorted(files))
 
         os.chdir(orig_cwd)
